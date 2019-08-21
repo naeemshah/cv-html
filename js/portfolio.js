a829cce728610b149e974e48313d2fb8a47d1da3
Naeem Shah <naeemshah45@gmail.com>
2019-08-21 12:47:39 +0500

    portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,12 +1,121 @@
 var portfolioItemJson = [
     { 
-      "name":"Visualize-Roi",
-      "ajax_link":"https://naeemshah.github.io/cv-html/portfolio/vroi.html",
-      "groups":"'all','media'",
+      "name":"Visualize-ROI",
+      "ajax_link":"http://localhost/cv/portfolio/vroi.html",
+      "groups":'"all", "nodejs", "reactjs", "php", "python", "yii"',
       "image":"images/portfolio/vroi.png",
       "tech":"PHP/Reacjs/Nodejs/Yii/Python"
 
+    },
+    { 
+      "name":"Custom Jackets",
+      "ajax_link":"http://localhost/cv/portfolio/vroi.html",
+      "groups":'"all", "wordpress", "reactjs", "php"',
+      "image":"images/portfolio/custom_jackets.png",
+      "tech":"PHP/Reacjs/Wordpress"
+
+    },
+    { 
+      "name":"Service Buddy",
+      "ajax_link":"http://localhost/cv/portfolio/vroi.html",
+      "groups":'"all", "laravel", "php"',
+      "image":"images/portfolio/service_buddy.png",
+      "tech":"PHP/Laravel/Javascript/Mobile/Stripe"
+
     }
+    ,
+    { 
+      "name":"Rapid Soft",
+      "ajax_link":"http://localhost/cv/portfolio/vroi.html",
+      "groups":'"all", "codeigniter", "php"',
+      "image":"images/portfolio/rapidsoft.png",
+      "tech":"PHP/codeigniter/HTML/CSS"
+
+    }
+    ,
+    { 
+      "name":"Livly",
+      "ajax_link":"http://localhost/cv/portfolio/vroi.html",
+      "groups":'"all", "magento", "php"',
+      "image":"images/portfolio/livly.png",
+      "tech":"PHP/Magento/Ecommerce"
+
+    }
+    ,
+    { 
+      "name":"GetWell3",
+      "ajax_link":"http://localhost/cv/portfolio/vroi.html",
+      "groups":'"all", "codeigniter", "php"',
+      "image":"images/portfolio/getwell3.png",
+      "tech":"PHP/Codeigniter/Ecommerce/Stripe"
+
+    }
+    ,
+    { 
+      "name":"StyleFinder",
+      "ajax_link":"http://localhost/cv/portfolio/vroi.html",
+      "groups":'"all", "wordpress", "php"',
+      "image":"images/portfolio/stylefinder.png",
+      "tech":"PHP/Wordpress/Ecommerce/Stripe"
+
+    }
+    ,
+    { 
+      "name":"FroalSupply",
+      "ajax_link":"http://localhost/cv/portfolio/vroi.html",
+      "groups":'"all", "magento", "php"',
+      "image":"images/portfolio/floralsupply.png",
+      "tech":"PHP/Magento/Ecommerce"
+
+    },
+    { 
+      "name":"Hipity",
+      "ajax_link":"http://localhost/cv/portfolio/vroi.html",
+      "groups":'"all", "php"',
+      "image":"images/portfolio/hipity.png",
+      "tech":"React Native/Nodejs/Wordpress/Joomla"
+
+    }
+    ,
+    { 
+      "name":"Royal Dental Clinic",
+      "ajax_link":"http://localhost/cv/portfolio/vroi.html",
+      "groups":'"all", "php"',
+      "image":"images/portfolio/royaldentalclinic.png",
+      "tech":"PHP/Wordpress/Ecommerce"
+
+    }
+    ,
+    { 
+      "name":"JK Instruments",
+      "ajax_link":"http://localhost/cv/portfolio/vroi.html",
+      "groups":'"all", "php"',
+      "image":"images/portfolio/jkinstruments.png",
+      "tech":"PHP/OpenCart/Ecommerce"
+
+    }
+    ,
+    { 
+      "name":"London Luggage",
+      "ajax_link":"http://localhost/cv/portfolio/vroi.html",
+      "groups":'"all", "php"',
+      "image":"images/portfolio/londonluggage.png",
+      "tech":"PHP/Wordpress/Ecommerce"
+
+    }
+    ,
+    { 
+      "name":"Solar Energy",
+      "ajax_link":"http://localhost/cv/portfolio/vroi.html",
+      "groups":'"all", "php"',
+      "image":"images/portfolio/solarenergy.png",
+      "tech":"PHP/Wordpress/Ecommerce"
+
+    }
+
+    
+
+
       ]
 
 var portfolioItemHtml   = document.getElementById("portfolio-item").innerHTML;
